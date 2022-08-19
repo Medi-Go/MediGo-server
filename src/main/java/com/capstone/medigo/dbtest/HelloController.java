1e02542b50d653a6d76e33defd3f034b98496ad6
Yonghoon Lee <dldydgns53@naver.com>
2022-08-20 05:51:54 +0900

    chore: DB 정보 암호화

--- src/main/java/com/capstone/medigo/dbtest/HelloController.java
+++ src/main/java/com/capstone/medigo/dbtest/HelloController.java
@@ -9,19 +9,9 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class HelloController {
-	private final MemberRepository memberRepository;
-	private final RedisTokenRepository redisTokenRepository;
 
 	@GetMapping("/")
 	public String hello() {
-		Member member = new Member("yong!");
-		memberRepository.save(member);
-		RedisToken redisToken = new RedisToken("value", 500000l);
-		redisTokenRepository.save(redisToken);
-
-		Member findMember = memberRepository.findById(1L).get();
-		RedisToken findRedis = redisTokenRepository.findById("value").get();
-
-		return "hello world" + findMember.getName() + findRedis.getTokenValue();
+		return "hello world";
 	}
 }
