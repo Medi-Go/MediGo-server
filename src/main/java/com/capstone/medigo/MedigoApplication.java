e613dd0a4af78fb8eb87f794ef68ae2ca13cbeea
Yonghoon Lee <dldydgns53@naver.com>
2022-08-19 22:16:00 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/capstone/medigo/MedigoApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.medigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/medigo/MedigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.medigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
