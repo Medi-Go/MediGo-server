a4479684a93cb45dadff8e3180ee316003af64d5
Yonghoon Lee <dldydgns53@naver.com>
2022-12-06 23:28:13 +0900

    fix: 1005데이터 갱신

--- src/main/java/com/capstone/medigo/domain/mydata/controller/MyDataTestController.java
+++ src/main/java/com/capstone/medigo/domain/mydata/controller/MyDataTestController.java
@@ -41,7 +41,7 @@ public class MyDataTestController {
 		save.changeMyDataLoadUpdateTime(LocalDateTime.of(1970, 01, 01, 00, 00, 00));
 		save.changeMyDataDetailUpdateTime(LocalDateTime.of(1970, 01, 01, 00, 00, 00));
 
-		String result = getResult("20221103");
+		String result = getResult("20221202");
 
 		myDataSaveService.save(result, save.getId());
 
@@ -53,7 +53,7 @@ public class MyDataTestController {
 	public String testData2() throws IOException {
 		Member save = memberRepository.findById(1L).get();
 
-		String result = get1005("20221123");
+		String result = get1005("20221212");
 
 		myDataSaveService.save(result, save.getId());
 
